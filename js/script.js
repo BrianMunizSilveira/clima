0c58eaf6ecd06b42e4cadab5f1e6817f10f8becd
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-11-13 10:51:56 -0300

    feat: layout, responsividade e funcionalidades aprimoradas
    - Adiciona animação de gradiente de fundo com transição suave;
    
    - Icones relacionados ao clima atual da cidade agora são animados;
    
    - Implementa a exibição de sugestões de cidades baseadas no continente selecionado;
    
    - Cria funcionalidade de busca de clima e imagens de cidades usando APIs externas;
    
    - Exibe informações detalhadas do clima, como temperatura, descrição, umidade e velocidade do vento;
    
    - Ajusta layout para ser responsivo, garantindo uma boa experiência em dispositivos móveis;
    
    - Melhora a interação com botões de busca e seleção de cidade;
    
    - Adiciona animações e transições para elementos interativos;
    
    - Ajusta cores e elementos visuais para otimizar a legibilidade e estética da página.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ const apikey = "SUA_CHAVE_AQUI"; // Adicione sua Chave da API da OpenWeatherMap 
 const accessKeyUnsplash = "SUA_CHAVE_AQUI"; // Adicione sua da Chave de API da Unsplash nesta linha.
 
 // Seleção de elementos
-
 const apiCountryURL = "https://flagsapi.com/";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
-
+const continentSelect = document.querySelector("#continent-select");
+const citySelect = document.querySelector("#city-select");
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
@@ -15,40 +15,121 @@ const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
 const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
-
 const weatherContainer = document.querySelector("#weather-data");
-
 const errorMessageContainer = document.querySelector("#error-message");
 const loader = document.querySelector("#loader");
-
 const suggestionContainer = document.querySelector("#suggestions");
-const suggestionButtons = document.querySelectorAll("#suggestions button");
+const showSuggestionsBtn = document.querySelector("#show-suggestions");
+const closeSuggestionsBtn = document.querySelector("#close-suggestions");
 
-// Loader
-const toggleLoader = () => {
-  loader.classList.toggle("hide");
+// Mapeamento de cidades por continente
+const citiesByContinent = {
+  america: [
+    "Nova York",
+    "Los Angeles",
+    "São Paulo",
+    "Buenos Aires",
+    "Toronto",
+    "Vancouver",
+    "Rio de Janeiro",
+    "México",
+    "Lima",
+    "Santiago",
+    "Caracas",
+    "Montréal",
+    "Bogotá",
+    "Quito",
+  ],
+  europa: [
+    "Paris",
+    "Londres",
+    "Roma",
+    "Madri",
+    "Berlim",
+    "Amsterdã",
+    "Lisboa",
+    "Praga",
+    "Budapeste",
+    "Viena",
+    "Zurique",
+    "Copenhague",
+    "Estocolmo",
+    "Bruxelas",
+    "Barcelona",
+    "Milão",
+    "Atenas",
+  ],
+  asia: [
+    "Tóquio",
+    "Pequim",
+    "Dubai",
+    "Seul",
+    "Bangkok",
+    "Hong Kong",
+    "Kuala Lumpur",
+    "Cingapura",
+    "Mumbai",
+    "Deli",
+    "Manila",
+    "Jacarta",
+    "Bangladesh",
+    "Moscovo",
+    "Calcutá",
+    "Yangon",
+    "Hanoi",
+  ],
+  africa: [
+    "Cidade do Cabo",
+    "Cairo",
+    "Lagos",
+    "Nairóbi",
+    "Joanesburgo",
+    "Casablanca",
+    "Dacar",
+    "Tunis",
+    "Abuja",
+    "Lagos",
+    "Addis Abeba",
+    "Accra",
+    "Algiers",
+    "Kigali",
+    "Lomé",
+    "Banjul",
+    "Marrakech",
+  ],
+  oceania: [
+    "Sydney",
+    "Melbourne",
+    "Auckland",
+    "Wellington",
+    "Suva",
+    "Port Vila",
+    "Fiji",
+    "Nouméa",
+    "Auckland",
+    "Honiara",
+    "Apia",
+    "Port Moresby",
+    "Papeete",
+    "Nadi",
+  ],
 };
 
-// Funções
+// Função para buscar os dados do clima
 const getWeatherData = async (city) => {
   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}&lang=pt_br`;
-
   const response = await fetch(apiWeatherURL);
   const data = await response.json();
-
   return data;
 };
 
-// Função para buscar várias imagens da cidade na Unsplash e selecionar uma aleatoriamente
-const getRandomUnsplashImage = async (city) => {
+// Função para buscar imagens da cidade
+const getCityImageFromUnsplash = async (city) => {
   const apiUnsplashURL = `https://api.unsplash.com/search/photos?query=${city}&per_page=5&client_id=${accessKeyUnsplash}`;
-
   try {
     const response = await fetch(apiUnsplashURL);
     const data = await response.json();
-
     if (data.results && data.results.length > 0) {
-      // Gerar um índice aleatório para selecionar uma das imagens
       const randomIndex = Math.floor(Math.random() * data.results.length);
       return data.results[randomIndex].urls.full;
     } else {
@@ -56,78 +137,74 @@ const getRandomUnsplashImage = async (city) => {
     }
   } catch (error) {
     console.error("Erro ao buscar imagem no Unsplash:", error);
-    return null; // Retorna null caso dê erro
+    return null;
   }
 };
 
-// Tratando erros
-const showErrorMessage = () => {
-  errorMessageContainer.classList.remove("hide");
+// Mapeamento de ícones SVG animados
+const weatherIconsMap = {
+  clear: "sunny.svg",
+  clouds: "cloudy.svg",
+  rain: "rainy.svg",
+  snow: "snow.svg",
+  drizzle: "drizzle.svg",
+  thunderstorm: "thunderstorm.svg",
 };
 
-const hideInformation = () => {
-  errorMessageContainer.classList.add("hide");
-  weatherContainer.classList.add("hide");
-
-  suggestionContainer.classList.add("hide");
-};
-
+// Função para mostrar os dados do clima
 const showWeatherData = async (city) => {
-  hideInformation();
-
   const data = await getWeatherData(city);
-
   if (data.cod === "404") {
-    showErrorMessage();
+    errorMessageContainer.classList.remove("hide");
     return;
   }
 
   cityElement.innerText = data.name;
   tempElement.innerText = parseInt(data.main.temp);
   descElement.innerText = data.weather[0].description;
-  weatherIconElement.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-  );
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed} Km/h`;
+
+  const weatherCondition = data.weather[0].main.toLowerCase();
+  const iconFile = weatherIconsMap[weatherCondition] || "default.svg";
+  weatherIconElement.setAttribute("src", `icons/${iconFile}`);
+
+  const countryCode = data.sys.country.toUpperCase();
   countryElement.setAttribute(
     "src",
-    apiCountryURL + data.sys.country + "/flat/64.png"
+    `${apiCountryURL}${countryCode}/flat/64.png`
   );
-  humidityElement.innerText = `${data.main.humidity}%`;
-  windElement.innerText = `${data.wind.speed}Km/h`;
 
-  // Buscar uma imagem aleatória da cidade na Unsplash
-  const imageUrl = await getRandomUnsplashImage(city);
-
+  const imageUrl = await getCityImageFromUnsplash(city);
   if (imageUrl) {
     document.body.style.backgroundImage = `url("${imageUrl}")`;
-  } else {
-    console.log("Imagem não encontrada para a cidade:", city);
   }
 
   weatherContainer.classList.remove("hide");
 };
 
-// Eventos
-searchBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  const city = cityInput.value;
-
-  showWeatherData(city);
-});
-
-cityInput.addEventListener("keyup", (e) => {
-  if (e.code === "Enter") {
-    const city = e.target.value;
-    showWeatherData(city);
+// Preencher as cidades com base no continente selecionado
+continentSelect.addEventListener("change", () => {
+  const continent = continentSelect.value;
+  citySelect.innerHTML = '<option value="">Selecione uma cidade</option>'; // Limpa as cidades
+  if (continent && citiesByContinent[continent]) {
+    citySelect.disabled = false;
+    citiesByContinent[continent].forEach((city) => {
+      const option = document.createElement("option");
+      option.value = city;
+      option.textContent = city;
+      citySelect.appendChild(option);
+    });
+  } else {
+    citySelect.disabled = true;
   }
 });
 
-// Sugestões
-suggestionButtons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const city = btn.getAttribute("id");
+// Evento para o botão de pesquisa
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const city = citySelect.value || cityInput.value;
+  if (city) {
     showWeatherData(city);
-  });
+  }
 });
